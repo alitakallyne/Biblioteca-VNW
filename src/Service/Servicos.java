c2caf7125613fd6a5f47aee24e8c06d161343a5e
alitakallyne <alytakallyne@gmail.com>
2024-03-06 22:39:38 -0300

    servicos

--- src/Service/Servicos.java
+++ src/Service/Servicos.java
@@ -2,6 +2,30 @@ package Service;
 
 public class Servicos {
 	
-	
+	private void cadastrarLivro(ArrayList<Livro> livros) {
+        
+        
+    }
+
+    private  void cadastrarUsuario(ArrayList<Usuario> usuarios) {
+        
+       
+    }
+
+    private  void listarLivros(ArrayList<Livro> livros) {
+    
+    }
+
+    private  void listarUsuarios(ArrayList<Usuario> usuarios) {
+
+    }
+
+    private  void realizarAluguel(ArrayList<Livro> livros, ArrayList<Usuario> usuarios, ArrayList<Aluguel> alugueis) {
+        
+    }
+
+    private  void devolverLivro(ArrayList<Aluguel> alugueis) {
+        
+    }
 
 }
